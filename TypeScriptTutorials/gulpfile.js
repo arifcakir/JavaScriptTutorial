60d6c6157905dff2c7cda47edfbcbee7b7d37b5e
murat cabuk <muratcabuk@live.com>
2019-03-19 17:54:12 +0300

    edited gulpfile

--- TypeScriptTutorials/gulpfile.js
+++ TypeScriptTutorials/gulpfile.js
@@ -1,67 +1,80 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
-var browserify = require("browserify");
-var source = require('vinyl-source-stream');
-var tsify = require("tsify");
+//var browserify = require("browserify");
+//var source = require('vinyl-source-stream');
+//var tsify = require("tsify");
 var less = require('gulp-less');
 var inject = require('gulp-inject');
 
 
 //farklı ts  moduller için bundan biekaçtane hazırlanabilir
-gulp.task("ts", function () {
-    return tsProject.src()
+function ts(done) {
+    
+    tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist/Scripts"));
-});
+
+    done();
+}
+exports.ts=ts;
 
 
-gulp.task("less", function () {
-    return gulp.src(["src/Styles/Main.less"]).
+function less(done) {
+    
+    gulp.src(["src/Styles/**/*.less"]).
         pipe(less()).
         //pipe(minifyCSS({})).
         pipe(gulp.dest("dist/Styles"));
-});
+    done();
+}
+exports.less=less;
 
 
-gulp.task("bootstrap", function () {
+function bootstrap(done) {
 
-    return gulp.src(["node_modules/bootstrap/dist/**/*"]).
-        pipe(gulp.dest("dist/ExtLib/bootstrap"))
-
-});
-
-gulp.task("jquery", function () {
-
-    return gulp.src(["node_modules/jquery/dist/*.js"]).
-        pipe(gulp.dest("dist/ExtLib/jquery/dist"))
+    gulp.src(["node_modules/bootstrap/dist/**/*"]).
+        pipe(gulp.dest("dist/ExtLib/bootstrap"));
+    done();
+}
+exports.bootstrap=bootstrap;
 
 
-});
+function jquery(done) {
+
+    gulp.src(["node_modules/jquery/dist/*.js"]).
+        pipe(gulp.dest("dist/ExtLib/jquery/dist"));
+    done();
+}
+exports.jquery=jquery;
 
 
-gulp.task("inject", function () {
+function injectIndex(done) {
 
     var css = gulp.src("dist/ExtLib/bootstrap/css/bootstrap.min.css");
     var js = gulp.src("dist/ExtLib/jquery/dist/jquery.min.js");
 
-    return gulp.src("src/index.html")
+    gulp.src("src/index.html")
         .pipe(inject(css, { relative: false }))
         .pipe(inject(js, { relative: false }))
         .pipe(gulp.dest("dist"));
-});
-
-gulp.task("injectHTMLs", function () {
-
-
+    done();
+}
+exports.injectIndex=injectIndex;
 
 
+function injectHTMLs(done) {
+
+    var js=gulp.src("/dist/Scripts/**/*.js");
+    
+    gulp.src("src/Scripts/**/*.html")
+      .pipe(inject(js, {relative: false}))
+      .pipe(gulp.dest("dist/Scripts"));
+    done();
+}
+exports.injectHTMLs=injectHTMLs;
 
 
-});
+exports.series = gulp.series(bootstrap, jquery, less, ts);
 
-
-
-gulp.task("default", gulp.series(["bootstrap", "jquery", "less", "ts", "inject"]));
-
-
+exports.parallel = gulp.parallel(injectHTMLs, injectIndex)
